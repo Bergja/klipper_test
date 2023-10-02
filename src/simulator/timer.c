5142deffbcd3cd54a6e88056cd14e87c81036fce
BergJack <5642689+BergJack@user.noreply.gitee.com>
2023-10-02 20:39:13 +0800

    recover timer

--- src/simulator/timer.c
+++ src/simulator/timer.c
@@ -4,14 +4,14 @@
 //
 // This file may be distributed under the terms of the GNU GPLv3 license.
 
-#include <time.h>            // struct timespec
-#include <unistd.h>          // usleep
-#include "autoconf.h"        // CONFIG_CLOCK_FREQ
-#include "board/irq.h"       // irq_disable
-#include "board/misc.h"      // timer_from_us
+#include <time.h> // struct timespec
+#include <unistd.h> // usleep
+#include "autoconf.h" // CONFIG_CLOCK_FREQ
+#include "board/irq.h" // irq_disable
+#include "board/misc.h" // timer_from_us
 #include "board/timer_irq.h" // timer_dispatch_many
-#include "command.h"         // DECL_CONSTANT
-#include "sched.h"           // DECL_INIT
+#include "command.h" // DECL_CONSTANT
+#include "sched.h" // DECL_INIT
 
 // Helper function that returns the system time as a 32bit counter
 static uint32_t
@@ -22,6 +22,7 @@ get_system_time(void)
     double t = (double)ts.tv_sec + (double)ts.tv_nsec * .000000001;
     return (uint32_t)(t * CONFIG_CLOCK_FREQ);
 }
+
 
 /****************************************************************
  * Timers
@@ -37,7 +38,8 @@ timer_read_time(void)
 }
 
 // Activate timer dispatch as soon as possible
-void timer_kick(void)
+void
+timer_kick(void)
 {
     next_wake_time = timer_read_time();
 }
@@ -49,23 +51,27 @@ do_timer_dispatch(void)
     next_wake_time = timer_dispatch_many();
 }
 
-void timer_init(void)
+void
+timer_init(void)
 {
     timer_kick();
 }
 DECL_INIT(timer_init);
+
 
 /****************************************************************
  * Interrupts
  ****************************************************************/
 
 // Disable hardware interrupts
-void irq_disable(void)
+void
+irq_disable(void)
 {
 }
 
 // Enable hardware interrupts
-void irq_enable(void)
+void
+irq_enable(void)
 {
 }
 
@@ -77,12 +83,14 @@ irq_save(void)
 }
 
 // Restore hardware interrupts to state from flag returned by irq_save()
-void irq_restore(irqstatus_t flag)
+void
+irq_restore(irqstatus_t flag)
 {
 }
 
 // Atomically enable hardware interrupts and sleep processor until next irq
-void irq_wait(void)
+void
+irq_wait(void)
 {
     // XXX - sleep to prevent excessive cpu usage in simulator
     usleep(1);
@@ -92,7 +100,8 @@ void irq_wait(void)
 
 // Check if an interrupt is active (used only on architectures that do
 // not have hardware interrupts)
-void irq_poll(void)
+void
+irq_poll(void)
 {
     uint32_t now = timer_read_time();
     if (!timer_is_before(now, next_wake_time))
